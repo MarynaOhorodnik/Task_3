12b6680307b17f9ab01ed2d1263643eb75d324f9
Maryna <m.ohorodnik.22@gmail.com>
2020-09-22 12:08:46 +0300

    Add project files.

---
+++ Task_3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b;
+            long c;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("(int) a = ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+
+                catch
+                {
+                    Console.WriteLine("It is not int. Input again.");
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("(double) b = ");
+                    b = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+
+                catch
+                {
+                    Console.WriteLine("It is not double. Input again.");
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("(long) c = ");
+                    c = Convert.ToInt64(Console.ReadLine());
+                    break;
+                }
+
+                catch
+                {
+                    Console.WriteLine("It is not long. Input again.");
+                }
+            }
+
+            Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+        }
+    }
+}
